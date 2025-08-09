9fcf8108e185f1111a7f27867b154642014f98ae
NicolasV99 <legolasnicolas99@gmail.com>
2025-08-09 11:48:38 -0500

    W06 Code: Trees Activity

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,29 +1,25 @@
-public class Node
-{
+public class Node{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data)
-    {
+    public Node(int data){
         this.Data = data;
     }
 
-    public void Insert(int value)
-    {
-        // TODO Start Problem 1
+    public void Insert(int value){
+        // TODO Start Problem 1 
+        if (value == Data){
+            return; 
+        }
 
-        if (value < Data)
-        {
-            // Insert to the left
+        if (value < Data){
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
-        {
-            // Insert to the right
+        else{
             if (Right is null)
                 Right = new Node(value);
             else
@@ -31,15 +27,42 @@ public class Node
         }
     }
 
-    public bool Contains(int value)
-    {
-        // TODO Start Problem 2
-        return false;
+    public bool Contains(int value){
+        // TODO Start Problem 2 
+        if (value == this.Data){
+            return true;
+        }
+        else if (value < this.Data){
+            if (this.Left is null){
+                return false;
+            }
+            else{
+                return this.Left.Contains(value);
+            }
+        }
+        else{
+            if (this.Right is null){
+                return false;
+            }
+            else{
+                return this.Right.Contains(value);
+            }
+        }
     }
 
-    public int GetHeight()
-    {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+     public int GetHeight(){
+        // TODO Start Problem 4 
+        
+        int leftHeight = 0;
+        if (Left is not null){
+            leftHeight = Left.GetHeight();
+        }
+
+        int rightHeight = 0;
+        if (Right is not null){
+            rightHeight = Right.GetHeight();
+        }
+        
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -9,8 +9,7 @@ public static class Trees
     /// a range (first to last) to consider.  For the first call, the full range of 0 to
     /// Length-1 used.
     /// </summary>
-    public static BinarySearchTree CreateTreeFromSortedList(int[] sortedNumbers)
-    {
+    public static BinarySearchTree CreateTreeFromSortedList(int[] sortedNumbers){
         var bst = new BinarySearchTree(); // Create an empty BST to start with 
         InsertMiddle(sortedNumbers, 0, sortedNumbers.Length - 1, bst);
         return bst;
@@ -46,8 +45,15 @@ public static class Trees
     /// <param name="first">the first index in the sortedNumbers to insert</param>
     /// <param name="last">the last index in the sortedNumbers to insert</param>
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
-    private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
-    {
+    private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst){
         // TODO Start Problem 5
+        if (first > last){
+            return;
+        }
+
+        int middle = first + (last - first) / 2;
+        bst.Insert(sortedNumbers[middle]);
+        InsertMiddle(sortedNumbers, first, middle - 1, bst);
+        InsertMiddle(sortedNumbers, middle + 1, last, bst);
     }
 }
